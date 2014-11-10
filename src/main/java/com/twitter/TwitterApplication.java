71d62eae4f0c46fe6228470f93403a8eb05d233e
Jonathan Gallimore <jgallimore@Jonathans-MacBook-Pro.local>
2014-11-10 23:22:13 +0000

    Reinstating @See links on app

--- src/main/java/com/twitter/TwitterApplication.java
+++ src/main/java/com/twitter/TwitterApplication.java
@@ -7,6 +7,11 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("api")
+@SeeAlso({
+        @See(title = "Uploading Media", iref = "uploading-media.html"),
+        @See(title = "Uploading Media: Multiple Photos", iref = "uploading-media-multiple-photo.html"),
+        @See(title = "The Search API", iref = "search-api.html")
+})
 public class TwitterApplication extends Application {
     // leave empty and let TomEE scan for @Path resources
 }
