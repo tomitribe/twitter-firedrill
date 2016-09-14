9c38691708b3f0954d346b61c9d38b5b545e2a2e
dblevins <david.blevins@gmail.com>
2016-09-13 23:07:05 -0700

    Nice technique to get random behavior out that is still "weighted" and looks more natural

--- src/main/java/com/twitter/ScenarioBuilder.java
+++ src/main/java/com/twitter/ScenarioBuilder.java
@@ -1,32 +1,28 @@
 package com.twitter;
 
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
+import java.util.function.Supplier;
 
 public class ScenarioBuilder {
 
-    /**
-     * Allows the following factors to be controlled
-     *  - Response code
-     *  - Invocation time
-     *  - Response bytes (size)
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
+    public static void technique1() {
 
-        final WeightedRandomResult<Integer> statusCodes = new WeightedRandomResult<Integer>(
-                200,
-                200,
-                200,
-                200,
-                500
-        );
-
-        final Integer integer = statusCodes.get();
-
-        final Runnable sleep = () -> {
+        final Runnable spike = () -> {
             try {
-                Thread.sleep(100);
+                Thread.sleep(100000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        final Runnable slow = () -> {
+            try {
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -41,31 +37,111 @@ public class ScenarioBuilder {
         };
 
         final WeightedRandomResult<Runnable> behavior = new WeightedRandomResult<Runnable>(
-                sleep,
-                sleep,
                 goodBehavior,
                 goodBehavior,
                 goodBehavior,
                 goodBehavior,
                 goodBehavior,
                 goodBehavior,
-                goodBehavior
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                goodBehavior,
+                slow,
+                slow,
+                slow,
+                slow,
+                spike
         );
 
+    }
+
+    public static void technique2() {
+
+        final List<Supplier<Response>> behaviors = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            behaviors.add(ScenarioBuilder::sleepOk);
+        }
+
+        for (int i = 0; i < 100; i++) {
+            behaviors.add(ScenarioBuilder::sortOfSlow);
+        }
+
+        for (int i = 0; i < 2; i++) {
+            behaviors.add(ScenarioBuilder::spike);
+        }
+
+        for (int i = 0; i < 12; i++) {
+            behaviors.add(() -> Response.serverError().build());
+        }
+
+        for (int i = 0; i < 12; i++) {
+            behaviors.add(() -> Response.status(403).build());
+        }
+
+        final WeightedRandomResult<Supplier<Response>> possibilities = new WeightedRandomResult<>(behaviors);
+
+        // Then later at runtime...
+        final Response response = possibilities.get().get();
+        // and then return it
+    }
+
+    private static Response sleepOk() {
+        try {
+            // note we'd still want some variance in response time
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Response.ok().build();
+    }
+
+    private static Response spike() {
+        try {
+            // note we'd still want some variance in response time
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Response.ok().build();
+    }
+
+    private static Response sortOfSlow() {
+        try {
+            // note we'd still want some variance in response time
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return Response.ok().build();
     }
 
     public static class WeightedRandomResult<T> {
         final Random random = new Random();
 
-        final T[] statusCodes;
+        final List<T> statusCodes = new ArrayList<>();
 
         public WeightedRandomResult(final T... statusCodes) {
-            this.statusCodes = statusCodes;
+            Collections.addAll(this.statusCodes, statusCodes);
+        }
+
+        public WeightedRandomResult(final Collection<T> statusCodes) {
+            this.statusCodes.addAll(statusCodes);
         }
 
         public T get() {
-            final int i = random.nextInt(statusCodes.length);
-            return statusCodes[i];
+            final int i = random.nextInt(statusCodes.size());
+            return statusCodes.get(i);
         }
     }
 
